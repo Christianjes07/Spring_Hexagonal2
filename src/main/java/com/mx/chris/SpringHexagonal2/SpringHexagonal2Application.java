2a3fb2883742bbcd713c228f36e7525eb8ebfa65
Christianjes07 <jesussaucedo896@gmail.com>
2025-10-02 13:04:18 -0600

    Initial commit

---
+++ src/main/java/com/mx/chris/SpringHexagonal2/SpringHexagonal2Application.java
@@ -0,0 +1,13 @@
+package com.mx.chris.SpringHexagonal2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHexagonal2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHexagonal2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mx/chris/SpringHexagonal2/SpringHexagonal2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.chris.SpringHexagonal2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHexagonal2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
