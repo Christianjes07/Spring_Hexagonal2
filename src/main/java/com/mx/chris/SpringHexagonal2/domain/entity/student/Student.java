48b5d823dff4ecd2a3036d5be77ce4ddfaa2dfd4
Christianjes07 <jesussaucedo896@gmail.com>
2025-10-02 13:23:30 -0600

    Student entity

---
+++ src/main/java/com/mx/chris/SpringHexagonal2/domain/entity/student/Student.java
@@ -0,0 +1,22 @@
+package com.mx.chris.SpringHexagonal2.domain.entity.student;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Student {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int status;
+
+}
